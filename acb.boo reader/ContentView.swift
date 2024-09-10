79edd290ae43d9126e65342e6003a85edc2f8665
Aidan Cornelius-Bell <aidan@cornelius-bell.com>
2024-09-10 15:50:42 +0930

    Initial Commit

---
+++ acb.boo reader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  acb.boo reader
+//
+//  Created by Aidan Cornelius-Bell on 10/9/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ acb.boo reader/acb_boo_readerApp.swift
@@ -0,0 +1,17 @@
+//
+//  acb_boo_readerApp.swift
+//  acb.boo reader
+//
+//  Created by Aidan Cornelius-Bell on 10/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct acb_boo_readerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
